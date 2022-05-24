86c88ba6f28e76250c34018c7259406dfa0fad0b
Vasilache Ionut <vasilacheorionut@gmail.com>
2022-05-24 18:03:49 +0200

    first commit

--- SQL_lotto_90_Italia/_db_09_aggiorna.sql
+++ SQL_lotto_90_Italia/_db_09_aggiorna.sql
@@ -7,4 +7,4 @@ CREATE TABLE `aggiorna` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `aggiorna` (`database`,`ultimo_aggiornamento`) VALUES ('lotto_90_Italia_1',NOW());
+INSERT INTO `aggiorna` (`database`,`ultimo_aggiornamento`) VALUES ('lotto_90_Italia',NOW());
